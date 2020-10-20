09d6f3ac8a80b7b6a0ea6635691aa08ca8d18525
Georgiy Lebedev <georgiyle@mail.ru>
2020-10-20 16:48:27 +0300

    [assembler_main.cpp]

--- assembler_main.cpp
+++ assembler_main.cpp
@@ -29,26 +29,25 @@ int main(int argc, const char *argv[])
         return EXIT_FAILURE;
     }
 
-    Assembler assembler;
-    if (assembler_ctor(&assembler, &text)) {
-        ERROR_OCCURRED_CALLING(assembler_ctor, "returned non-zero value");
-        text_dtor(&text);
-        printf("Failed to assemble input file, check dump.log for more information\n");
-        return EXIT_FAILURE;
-    }
-
     char *output_file_name = (char *) calloc(strlen(input_file_name) + strlen("meow") - strlen("asm") + 1, sizeof(char));
 
     if (output_file_name == NULL) {
         ERROR_OCCURRED_CALLING(calloc, "returned NULL");
         text_dtor(&text);
-        assembler_dtor(&assembler);
         printf("Failed to assemble input file, check dump.log for more information\n");
         return EXIT_FAILURE;
     }
 
     strcpy(output_file_name, input_file_name);
-    strcpy(strchr(output_file_name, '.'), ".meow");
+    strcpy(strchr(output_file_name, '.') + 1, "meow");
+
+    Assembler assembler;
+    if (assembler_ctor(&assembler, &text, input_file_name, output_file_name)) {
+        ERROR_OCCURRED_CALLING(assembler_ctor, "returned non-zero value");
+        text_dtor(&text);
+        printf("Failed to assemble input file, check dump.log for more information\n");
+        return EXIT_FAILURE;
+    }
 
     if (assembler_translate_text(&assembler)) {
         ERROR_OCCURRED_CALLING(assembler_translate_text, "returned non-zero value");
